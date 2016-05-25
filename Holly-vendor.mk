6ef11966af2e8f54d6d7e74f243a04a309017dc3
DarkKnight6499 <yazad.madan@gmail.com>
2016-05-25 17:01:46 +0530

    M bringup

---
+++ Holly-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/Holly/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/Holly/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/Holly/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/Holly/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Huawei/Holly/xbin,system/xbin)
+
